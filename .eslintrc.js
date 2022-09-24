2e0faebc6a03882b486480b0de4f7dfcc1dc729b
Elizabeth <dun1isa@yandex.ru>
2022-09-24 16:28:54 +0300

    eslint problms

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,9 @@ module.exports = {
     "react"
   ],
   "rules": {
-  },
-  "no-underscore-dangle": [
-    "error", { "allow": ["_id"] }
-  ]
+    "no-underscore-dangle": [
+      "error",
+      { "allow": ["_id"] }
+    ]
+  }
 }
